e5a01e1fafd97f9533c8f9e4fef32223edc70551
dimknysh <dimknysh@gmail.com>
2023-11-12 17:22:43 +0300

    123

---
+++ src/main/java/hibernate_test2/Test1.java
@@ -0,0 +1,26 @@
+package hibernate_test2;
+
+import hibernate_test2.entity.Detail;
+import hibernate_test2.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        try (SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+             Session session = factory.getCurrentSession();
+        ) {
+            session.beginTransaction();
+
+            Employee emp = session.get(Employee.class, 2);
+           session.delete(emp);
+
+            session.getTransaction().commit();
+        }
+    }
+}
